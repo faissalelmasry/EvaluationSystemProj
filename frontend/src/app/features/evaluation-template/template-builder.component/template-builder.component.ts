ba697c1eac68e88638ed8226481f11941b25ca99
Faissal Mahmoud <149112005+faissalelmasry@users.noreply.github.com>
2026-06-11 20:51:37 +0300

    configuring folders of Angular feature based architecture

---
+++ frontend/src/app/features/evaluation-template/template-builder.component/template-builder.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-builder.component',
+  imports: [],
+  templateUrl: './template-builder.component.html',
+  styleUrl: './template-builder.component.scss',
+})
+export class TemplateBuilderComponent {}
---
+++ frontend/src/app/features/evaluation-template/template-list.component/template-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-list.component',
+  imports: [],
+  templateUrl: './template-list.component.html',
+  styleUrl: './template-list.component.scss',
+})
+export class TemplateListComponent {}
